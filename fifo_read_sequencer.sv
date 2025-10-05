ddc025b0365fed6490b34b22587075a1a74d6b2a
Sathwik Bhat <109812189+sathwikb98@users.noreply.github.com>
2025-10-05 17:03:42 +0530

    Delete fifo_read_sequencer.sv

--- fifo_read_sequencer.sv
+++
@@ -1,6 +0,0 @@
-class fifo_read_sequencer extends uvm_sequencer#(fifo_read_seq_item);
-  `uvm_component_utils(fifo_read_sequencer)
-  function new(string name="fifo_read_sequencer",uvm_component parent =null);
-    super.new(name,parent);
-  endfunction
-endclass
